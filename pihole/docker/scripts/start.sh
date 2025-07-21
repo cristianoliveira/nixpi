c81c7b375b35d1fe70b4a8f6253256eb30ec3434
cristianoliveira <contato@cristianoliveira.com.br>
2025-07-21 21:45:31 +0000

    fix: issue with docker start script

--- pihole/docker/scripts/start.sh
+++ pihole/docker/scripts/start.sh
@@ -6,18 +6,15 @@ source /etc/pihole/environment
 WEBPASS=${PIHOLE_WEBPASS?"Missing password definition: PIHOLE_WEBPASS"}
 SERVERIP=${PIHOLE_SEVICEIP?"Missing pihole server ip address: PIHOLE_SEVICEIP"}
 
-docker run  \
-  --name pihole \
+docker run --name pihole \
   -p 53:53/tcp \
   -p 53:53/udp \
-  -p 80:80/tcp \
-  -p 443:443/tcp \
-  -e TZ=Europe/London \
+  -p 8053:80/tcp \
+  -e TZ=Europe/Berlin \
   -e FTLCONF_webserver_api_password="$WEBPASS" \
   -e FTLCONF_dns_listeningMode=all \
-  -v /etc/pihole:/etc/pihole \
-  -v /etc/dnsmasq.d:/etc/dnsmasq.d \
-  --cap-add NET_ADMIN \
+  -v ./etc/pihole:/etc/pihole \
+  -v ./etc/dnsmasq.d:/etc/dnsmasq.d \
   --restart unless-stopped \
-  --dns=1.1.1.1 --dns=1.0.0.1 \
   pihole/pihole:latest
+
