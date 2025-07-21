0675b754867aea7910baba80e7f6d9da66ffe7c5
cristianoliveira <contato@cristianoliveira.com.br>
2025-07-21 15:44:08 +0000

    fix: issue with routing dns

--- pihole/docker/scripts/start.sh
+++ pihole/docker/scripts/start.sh
@@ -6,16 +6,18 @@ source /etc/pihole/environment
 WEBPASS=${PIHOLE_WEBPASS?"Missing password definition: PIHOLE_WEBPASS"}
 SERVERIP=${PIHOLE_SEVICEIP?"Missing pihole server ip address: PIHOLE_SEVICEIP"}
 
-docker run --name pihole \
-  --restart=unless-stopped \
-  -p 53:53/tcp -p 53:53/udp \
-  -p 4080:80/tcp \
+docker run  \
+  --name pihole \
+  -p 53:53/tcp \
+  -p 53:53/udp \
+  -p 80:80/tcp \
   -p 443:443/tcp \
-  -e TZ='Europe/Berlin' \
-  -e WEBPASSWORD="$WEBPASS" \
-  -e SERVERIP="$SERVERIP" \
-  -v /var/lib/pihole/etc-pihole:/etc/pihole \
-  -v /var/lib/pihole/etc-dnsmasq.d:/etc/dnsmasq.d \
+  -e TZ=Europe/London \
+  -e FTLCONF_webserver_api_password="$WEBPASS" \
+  -e FTLCONF_dns_listeningMode=all \
+  -v /etc/pihole:/etc/pihole \
+  -v /etc/dnsmasq.d:/etc/dnsmasq.d \
+  --cap-add NET_ADMIN \
+  --restart unless-stopped \
   --dns=1.1.1.1 --dns=1.0.0.1 \
-  --cap-add=NET_ADMIN \
   pihole/pihole:latest
