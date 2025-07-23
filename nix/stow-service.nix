4bf5e666ae6c6aa1b73041ff98f76300e7766965
cristianoliveira <contato@cristianoliveira.com.br>
2025-07-23 10:58:35 +0200

    fix: avoid using env variables

--- nix/stow-service.nix
+++ nix/stow-service.nix
@@ -1,11 +1,12 @@
-{ pkgs ? import <nixpkgs> {}, ... }: let 
-in {
+{ pkgs ? import <nixpkgs> {}, ... }: {
+
   # Run stow on login to apply symlinks
+
   systemd.services.stow = {
-    description = "...";
+    description = "Stow Configuration Service";
 
     script = ''
-      ${pkgs.stow}/bin/stow -d /home/cris/nixpi/stow -t $HOME
+      ${pkgs.stow}/bin/stow -d /home/cris/nixpi/stow -t /home/cris
     '';
 
     wantedBy = [ "multi-user.target" ]; # starts after login
