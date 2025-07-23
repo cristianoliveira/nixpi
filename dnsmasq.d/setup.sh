a201ee9e24541eb979fd36e86bfbeb23c129eda1
cristianoliveira <contato@cristianoliveira.com.br>
2025-07-23 18:07:50 +0200

    feat: adds local.lab for localhost

--- dnsmasq.d/setup.sh
+++ dnsmasq.d/setup.sh
@@ -13,4 +13,7 @@ sudo mkdir /etc/dnsmasq.d
 CURRENT_IP=$(ip addr show | grep 'inet 192' | awk '{print $2}' | cut -d'/' -f1)
 echo "Current IP: $CURRENT_IP"
 
-sudo echo "address=/nixpi.lab/$CURRENT_IP" > /etc/dnsmasq.d/01-catchall.conf
+sudo cat > /etc/dnsmasq.d/01-catchall.conf <<EOF
+address=/nixpi.lab/$CURRENT_IP
+address=/local.lab/127.0.0.1
+EOF
