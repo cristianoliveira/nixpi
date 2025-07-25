a24d7bd3fecf465b0f9a235aec965bd0d2f25d1b
cristianoliveira <contato@cristianoliveira.com.br>
2025-07-25 08:11:05 +0200

    chore(pihole): remove unused scripts

--- pihole/docker/scripts/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-sudo mkdir -p /etc/pihole
-sudo mkdir -p /opt/pihole
-
-sudo ln -sf $HOME/nixpi/pihole/docker/scripts /opt/pihole/
--- pihole/docker/scripts/stop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-docker stop pihole || echo "already stopped"
-docker rm -f pihole
